2cbe6ccc401e13bfcee653e126a32b6b6ce82317
Nguyen Minh Ngoc <ngoc2012@yahoo.com>
2024-01-18 11:16:12 +0100

    start bug

--- backend/pong/consumers.py
+++ backend/pong/consumers.py
@@ -38,9 +38,7 @@ class PongConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-        if text_data == 'start':
-            self.room = RoomsModel.objects.get(id=room_id)
-            if not self.room.started:
+        if text_data == 'start' and not self.room.started:
                 asyncio.create_task(self.game_loop())
         elif text_data == 'left':
             await left(self)
