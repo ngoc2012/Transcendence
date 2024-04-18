fe1bafe645f5ff3920c8a962c25c85f67bfeac97
Araein <lebouvier.theo1@google.com>
2024-04-18 21:55:20 +0200

    index front better

--- frontend/index.js
+++ frontend/index.js
@@ -15,12 +15,13 @@ if (my42login !== null && my42login !== "" && my42email !== "" && my42ws != "")
     var dom_log_in = document.getElementById('login');
     if (dom_log_in) {
         dom_log_in.style.display = "none";
+        dom_login.insertAdjacentHTML('afterend', '<button id="logoutButton" class="btn btn-danger">Logout</button>');
     }
 
     var dom_signup = document.getElementById('signup');
     if (dom_signup) {
         dom_signup.style.display = "none";
-        dom_signup.insertAdjacentHTML('afterend', '<button id="logoutButton" class="btn btn-danger">Logout</button>');
+        // dom_signup.insertAdjacentHTML('afterend', '<button id="logoutButton" class="btn btn-danger">Logout</button>');
     }
 
     var dom_logout = document.getElementById('logoutButton');
